ea8aaf9d35eca7a1b91f0195ae5b24d6c7d5d809
Lisa Cee <three_nineteen@yahoo.com>
2017-05-25 16:42:02 -0700

    added gallery to html and played with css and js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   $('.center').slick({
-		centerMode: false,
-		centerPadding: '60px',
-		slidesToShow: 3,
-		autoplay: true,
-		arrows: false,
+    centerMode: false,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    autoplay: true,
+    arrows: false,
   });
-	 var small={width: "200px",height: "116px"};
-        var large={width: "400px",height: "232px"};
+   var small={width: "100%", height: "100%"};
+        var large={width: "150%",height: "600px"};
         var count=1;
         $(".slide").css(small).on('click',function () {
             $(this).animate((count==1)?large:small);
@@ -15,6 +15,7 @@ $(document).ready(function(){
         });
 })
 
+
 // $("img").click(function(){
-// 	$(this).css("width", "125%");
-// 	});
+//  $(this).css("width", "125%");
+//  });
