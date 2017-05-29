9a2987eb02984108f094137424c86be86c9cad96
Lisa Cee <three_nineteen@yahoo.com>
2017-05-29 14:47:23 -0700

    added lightbox

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ $(document).ready(function(){
     arrows: false,
   });
 
-   var small={width: "100%", height: "100%"};
-        var large={width: "150%",height: "600px"};
-        var count=1;
-        $(".slide").css(small).on('click',function () {
-            $(this).animate((count==1)?large:small);
-            count = 1-count;
-        });
+
 })
 
-
-// $("img").click(function(){
-//  $(this).css("width", "125%");
-//  });
